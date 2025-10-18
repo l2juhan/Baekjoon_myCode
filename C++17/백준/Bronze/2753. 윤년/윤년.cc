97d513b35aa6316f8f2f19d9a6ed0d439e41d711
l2juhan <80254217+l2juhan@users.noreply.github.com>
2025-10-18 21:10:47 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int y;
+    bool ans = false;
+    cin >> y;
+    if(((y % 4 == 0) && (y % 100 != 0)) || (y % 400 == 0 )){
+        ans = true;
+    }
+    cout << ans;
+}
