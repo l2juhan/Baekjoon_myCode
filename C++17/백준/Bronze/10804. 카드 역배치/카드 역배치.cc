2552b828e079469192fc1e1ca7bd410ef86c0f8a
l2juhan <80254217+l2juhan@users.noreply.github.com>
2025-10-18 21:34:38 +0900

    [Bronze II] Title: 카드 역배치, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10804. 카드 역배치/카드 역배치.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+//-------------------------------------------------------------------------
+
+    int arr[21]={};
+    for (int i=1;i<=20;i++) {
+        arr[i]=i;
+    }
+
+    for (int i=0;i<10;i++) {
+        int a,b;
+        cin >> a >> b;
+        reverse(arr+a,arr+b+1);
+    }
+    for (int i=1;i<=20;i++) {
+        cout << arr[i] << ' ';
+    }
+
+//-------------------------------------------------------------------------
+}
