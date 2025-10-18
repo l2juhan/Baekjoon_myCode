46c29e8ed7b8049697e7f8a3d211e424c68c2729
l2juhan <80254217+l2juhan@users.noreply.github.com>
2025-10-18 21:44:19 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    
+    int n;
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++) cout << " ";
+        for(int j=1;j<=2*i-1;j++) cout << "*";
+        cout << endl;
+    }
+    
+    return 0;
+}
