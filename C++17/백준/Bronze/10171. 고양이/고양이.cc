6ad1b0dc7efe7473afe6a269301f79f4b2a41d23
l2juhan <80254217+l2juhan@users.noreply.github.com>
2025-10-18 21:09:44 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(){
+    cout << "\\    /\\" << endl;
+    cout << " )  ( \')" << endl;
+    cout << "(  /  )" << endl;
+    cout << " \\(__)|";
+}
