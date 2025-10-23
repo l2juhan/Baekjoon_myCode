872e963ede7bd8c9476c09eaa42553d7c71fa1c0
l2juhan <80254217+l2juhan@users.noreply.github.com>
2025-10-23 16:23:21 +0900

    [Gold IV] Title: N-Queen, Time: 1644 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Gold/9663. N－Queen/N－Queen.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+    //-----------------------------
+
+
+int n;
+int ans;
+
+bool isused1[40];
+bool isused2[40];
+bool isused3[40];
+
+void func(int k) {
+    if (k==n) {
+        ans++;
+        return;
+    }
+    for (int i=0;i<n;++i) {
+        if (isused1[i]||isused2[i+k]||isused3[i-k+n+1])continue;
+        isused1[i]=1;
+        isused2[i+k]=1;
+        isused3[i-k+n+1]=1;
+        func(k+1);
+        isused1[i]=0;
+        isused2[i+k]=0;
+        isused3[i-k+n+1]=0;
+    }
+}
+
+    //-----------------------------
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    //-----------------------------//
+    cin>>n;
+    func(0);
+    cout<<ans;
+    //-----------------------------//
+    return 0;
+    }
