f000301ac1a4cbcf377f6b2e96b1e21367ec8edc
l2juhan <80254217+l2juhan@users.noreply.github.com>
2025-10-23 16:25:31 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+    //-----------------------------
+int N,M;
+vector<int>s;
+int arr[10];
+bool isUsed[10];
+void func(int k,int idx) {
+    if (k==M) {
+        for (int i=0;i<M;++i) {
+            cout<<arr[i]<<' ';
+        }
+        cout<<'\n';
+        return;
+    }
+    int prev_num = 0;
+    for (int i=idx;i<N;++i) {
+        if (!isUsed[i]&&prev_num!=s[i]) {
+            arr[k]=s[i];
+            prev_num=s[i];
+
+            func(k + 1,i);
+            isUsed[i] = false;
+        }
+
+    }
+}
+    //-----------------------------
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    //-----------------------------//
+    cin>>N>>M;
+   for (int i=0;i<N;++i) {
+        int a; cin>>a;
+        s.push_back(a);
+    }
+    sort(s.begin(),s.end());
+
+    func(0,0);
+    //-----------------------------//
+    return 0;
+    }
