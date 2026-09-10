516ef12682d9a087cc6e70f1d10a662e3e62eace
l2juhan <80254217+l2juhan@users.noreply.github.com>
2026-09-10 15:56:44 +0900

    [level 2] Title: [1차] 캐시, Time: 11.49 ms, Memory: 12.6 MB -BaekjoonHub

--- C++/프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.cpp
+++ C++/프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.cpp
@@ -1,21 +1,29 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+// 더 좋은 풀이
 
 int solution(int cacheSize, vector<string> cities) {
+    if (cacheSize == 0) return 5 * (int)cities.size();
+
+    list<string> lru;  // front = 가장 최근 사용
+    unordered_map<string, list<string>::iterator> pos;
     int answer = 0;
-    if(cacheSize==0) return 5*(int)cities.size();
-    deque<string> cache(cacheSize);
-    for(int i=0;i<(int)cities.size();++i){
-        string s=cities[i];
-        transform(s.begin(),s.end(),s.begin(),[](unsigned char c){return tolower(c);});
-        auto it=find(cache.begin(),cache.end(),s);
-        if(it==cache.end()){
-            cache.pop_front(); cache.push_back(s);
-            answer+=5;
-        }else{
-            cache.erase(it); cache.push_back(s);
-            answer+=1;
+
+    for (auto& s : cities) {
+        for (auto& c : s) c = tolower((unsigned char)c);
+
+        auto it = pos.find(s);
+        if (it != pos.end()) {
+            lru.splice(lru.begin(), lru, it->second);  // O(1) 이동, iterator 유효 유지
+            answer += 1;
+        } else {
+            if ((int)lru.size() == cacheSize) {
+                pos.erase(lru.back());
+                lru.pop_back();
+            }
+            lru.push_front(s);
+            pos[s] = lru.begin();
+            answer += 5;
         }
     }
     return answer;
